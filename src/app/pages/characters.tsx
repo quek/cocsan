5199ac1cc10633f9e3bcf6b768a2949697f08da5
TAHARA Yoshinori <read.eval.print@gmail.com>
2019-04-27 09:58:55 +0900

    .

--- src/app/pages/characters.tsx
+++
@@ -1,7 +0,0 @@
-import App from '../components/App';
-
-export default () => (
-  <App>
-    <h1>キャラクター</h1>
-  </App>
-);
---
+++ src/app/pages/characters/index.tsx
@@ -0,0 +1,9 @@
+import App from '../../components/App';
+import Characters from '../../components/Characters';
+
+export default () => (
+  <App>
+    <h1>キャラクター</h1>
+    <Characters />
+  </App>
+);
