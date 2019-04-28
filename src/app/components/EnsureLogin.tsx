935e65752580d427cb120621dc3e36a3f9be7821
TAHARA Yoshinori <read.eval.print@gmail.com>
2019-04-28 11:00:03 +0900

    .

--- src/app/components/EnsureLogin.tsx
+++ src/app/components/EnsureLogin.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
+import firebase from 'firebase/app';
 import loadFirebase from '../loadFirebase';
 
 export default class EnsureLogin extends React.Component<{}> {
   public render() {
-    return null;
+    return <button onClick={this.logout}>ログアウト</button>;
   }
+
   public async componentDidMount() {
     await loadFirebase();
+    const unsubscribe = firebase
+      .auth()
+      .onAuthStateChanged(async firebaseUser => {
+        console.log('onAuthStateChanged');
+        unsubscribe();
+        if (firebaseUser) {
+          console.log(firebaseUser);
+        } else {
+          const provider = new firebase.auth.GoogleAuthProvider();
+          firebase.auth().signInWithRedirect(provider);
+        }
+      });
+
+    firebase
+      .auth()
+      .getRedirectResult()
+      .then(function(result) {
+        // if (result.credential) {
+        //   // This gives you a Google Access Token. You can use it to access the Google API.
+        //   var token = result.credential.accessToken;
+        //   // ...
+        // }
+        // The signed-in user info.
+        var user = result.user;
+        console.log('getRedirectResult');
+        console.log(user);
+      });
   }
+
+  private logout = async () => {
+    await loadFirebase();
+    await firebase.auth().signOut();
+  };
 }
